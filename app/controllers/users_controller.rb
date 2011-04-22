c6fcfd8ee9e8d0ccac9477f94c524d1dbb42bcda
Kreationz <kreationz409@gmail.com>
2011-04-22 02:51:42 +0100

    User Signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,16 +41,13 @@ class UsersController < ApplicationController
   # POST /users.xml
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        flash[:success] = "Welcome to Your Personal Collection App!!"
+        redirect_to @user
+        else
+        @title = "Sign up"
+        render 'new'
       end
-    end
   end
 
   # PUT /users/1
@@ -75,6 +72,7 @@ class UsersController < ApplicationController
   end
     
   def new
+    @user = User.new
     @title = "Sign up"
   end
 
